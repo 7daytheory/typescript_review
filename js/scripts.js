01732155b1e503a32611667328a64c2fd98e42ee
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-02-17 16:33:19 -0500

    Overview of statically typed and dynamically typed code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 //Custom Js
+
+/* Statically Typed (C++ , C+ , Java)
+int number = 10;
+number = "a"; Causes Error obviously
+
+Dynamically Typed (Javascript, Python, Ruby)
+let number = 10;
+number = "a"; // No Error - determines variable type at runtime
+Math.round(number) // Would then cause an error since the number is now a string
+
